abbf66f60db5febe4639f9d997dce8728a4508b6
Andrice Nichols <johnnytes10@gmail.com>
2021-04-22 15:38:14 -0400

    Object equality practice

--- javascript-practice/practice.js
+++ javascript-practice/practice.js
@@ -1,31 +1,28 @@
-const address = showAddress('34 Northumberland Dr.', 'Mount Holly', '08060')
+// Defined objects then created constructed function
+let address1 = new Address('a', 'b', 'c')
+let address2 = new Address('a', 'b', 'c')
 
-// Factory Function
-function showAddress(street, city, zipCode) {
-    return {
-        street,
-        city,
-        zipCode
-    };
-}
-console.log(address)
-///////////////////////////////////////////////
-
-
-// Constructor Function
-
-
-function Address(street, city, zipCode){
-    this.street = street;
-    this.city = city;
+function Address(street,city,zipCode){
+    this.street = street,
+    this.city = city,
     this.zipCode = zipCode
 }
+////////////////////////////////////////////////////////
+//Developed Functions to determine object reference equality and property equality
+function areSame(address1, address2){
+   return address1 === address2
+}
 
-const address2 = new Address('34 Northumberland Dr.', 'Mount Holly', '08060')
-
-console.log(address2)
 
 
-// What I Learned:
-//The differences between a constructor and factory function as well as how to 
-//Implement new objects
+function areEqual(address1, address2){
+    return address1.street === address2.street &&
+    address1.city === address2.city && 
+    address1.zipCode === address2.zipCode
+}
+
+console.log(areSame(address1, address2))
+console.log(areEqual(address1, address2))
+
+//These functions were made to determine whether the constructed functions
+// were equal in terms of object value and the same regarding reference value
