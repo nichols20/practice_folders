850503d18ad32ea6c9a1bf0368f18538b420068f
Andrice Nichols <johnnytes10@gmail.com>
2021-04-27 23:18:58 -0400

    added try/catch to occurence if array is not array

--- JS-Completed-Exercises/count-occurences.js
+++ JS-Completed-Exercises/count-occurences.js
@@ -1,16 +1,25 @@
-const numbers = [1,2, 3, 4, 1, 1];
 
-const output = countOccurences(numbers, 1);
-
-console.log(output)
 function countOccurences(array, searchElement){
+    if (!Array.isArray(array)){
+        throw new Error('Value defined is not an Array')
+    }
     const search = [...array]
     const found = []
 
-    console.log(search.forEach(value => { 
+    search.forEach(value => { 
         const br = value === searchElement
         if (br === true)found.push(searchElement)
     }
-    ))
+    )
     console.log(found.length)
+}
+
+
+try{
+    const numbers = [1,2, 3, 4, 1, 1];
+    countOccurences(numbers, 1);
+}
+
+catch(e){
+    console.log(e)
 }
