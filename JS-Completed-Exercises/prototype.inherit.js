8cdb98d74d773a4fb794a01b631275b8c65de3ec
nichols20 <johnnytes10@gmail.com>
2021-05-06 23:57:57 -0400

    polymorphism practice

--- JS-Completed-Exercises/prototype.inherit.js
+++ JS-Completed-Exercises/prototype.inherit.js
@@ -1,76 +1,84 @@
 
-function Shape(color){
-    this.color = color; /*added a property that should be inherited by all shapes */
+
+function HtmlElement(){
+    this.click = function(){
+        console.log('clicked')
+    }
 }
 
-Shape.prototype.duplicate = function(){
-    console.log('duplicate')
+HtmlElement.prototype.focus = function(){
+    console.log('focus')
 }
 
-function extend(Child, Parent){
-    Child.prototype = Object.create(Parent.prototype)
-    Child.prototype.constructor = Child;
-} /* in this lesson I learned About intermediate function inheritance, I transfered 
-over the code that reset the prototype of a constructor function into a new function
-labeled extend that way I wouldn't have to retype those same two lines every time 
-I decide to have a function inherit the Shape function. Now when needed I call extend 
-(a,b) to whatever function and it automatically inherits the Shape prototype. examples
-can be found in line 26 and 37 */
-function Circle(radius, color){
-    Shape.call(this, color); /* learned how to call super constructors used the 
-                             call method to access the this.color property 
-                             and attribute it to the circle function*/
-    this.radius = radius
+function HtmlSelectElement(...items){
+    this.items = items
+    this.addItem = function(add){
+        this.items.push(add)
+    }
+    this.removeItem = function(remove){
+        const index = this.items.indexOf(remove)
+        this.items.splice(index, 1)
+    }
+    /* I created this render method as a demonstration of polymorphism this render method 
+    will take all items defined and create individual elements per value stated inside of a for loop.
+    before this occurs I created A parent element that will house each individual element inside of itself finally 
+    returning the complete element as the result */
+    this.render = function(){                                    
+        const element = document.createElement('select')        
+        for (item of this.items){
+            const childElement = document.createElement('option')
+            const elementContent = document.createTextNode(item)
+            childElement.appendChild(elementContent)
+            element.appendChild(childElement)
+        }
+        return element
+    }
+} 
+
+function HtmlImageSelect(sources){
+    HtmlSelectElement.call(this)
+    delete this.items
+    delete this.addItem
+    delete this.removeItem
+    this.sources = sources
+    this.render = function(){
+        this.sources = sources 
+        return `<img src="${this.sources}" />`
+    }
 }
 
-extend(Circle, Shape)
+HtmlSelectElement.prototype = new HtmlElement /* correct implementation */
 
-Circle.prototype.duplicate = function(){
-    Shape.prototype.duplicate.call(this) /* if you wish to still call the first
-    implementation of the parent prototype you can do so by using the call method */
+HtmlImageSelect.prototype = new HtmlElement
+HtmlImageSelect.prototype.constructor = HtmlImageSelect
 
-    console.log('duplicate circle')
-}/* used the method override method to change the duplicate function for the circle
-constructor after the circle constructor had inherited the initial properties of
-the shape constructor function */
+const e = new HtmlElement
 
-Circle.prototype.draw = function(){
-    console.log('draw')
+const c = new HtmlSelectElement(1,2,3)
+
+const img = new HtmlImageSelect('https')
+
+HtmlSelectElement.prototype = new HtmlElement/* Incorrect implementation */
+
+const elements = [
+    new HtmlSelectElement(1,2,3),
+    new HtmlImageSelect('loser')
+];
+
+for (let element of elements){
+    console.log(element.render())
 }
 
-function Square(size, color){
-    this.size = size
-
-    Shape.call(this, color);
-}
-
-extend(Square, Shape)
-
-/* duplicated circle.prototype.duplicate for square object */
-
-Square.prototype.duplicate = function(){
-    Shape.prototype.duplicate.call(this) 
-    console.log('duplicate Square')
-}
+/* in this exercise I practiced creating my own protoypical inheritance,
+overall this exercise was fairly simple when it came to applying the prototypes of 
+individual constructor functions however I began to face problems when 
+attempting to assign HtmlSelectElements prototype to the HtmlElement 
+constructor function. This is because I had Defined the new prototype 
+after I had already defined c = new HtmlSelectElement. Forgetting
+that there is no hoisting in this scenario I was confused as to why the code
+wasn't working not realizing I had to just move it above the defined c
+object to fix this issue. My takeaway is the reminder that there is no hoisting
+for constructor functions and if i wish to reassign the prototype of a constructor
+function I must do it before defining the object containing said function */
 
 
-const shapes = [
-    new Circle,
-    new Square
-]
-
-/* added this for of loop for an example of polymorphism, what this did
-was allow me to call multiple different forms of the shape object
-and call each objects duplicate function by morhphing it into one line
-of code. Instead of having to wrtie multiple individual if statements
-depending on the type of shape.*/
-for (let shape of shapes){
-    shape.duplicate();
-}
-
-
-const s = new Shape
-
-const c = new Circle(1, 'red')
-
-const squares = new Square(2, 'blue')
--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -1,5 +1,87 @@
 
 
+function HtmlElement(){
+    this.click = function(){
+        console.log('clicked')
+    }
+}
+
+HtmlElement.prototype.focus = function(){
+    console.log('focus')
+}
+
+function HtmlSelectElement(...items){
+    this.items = items
+    this.addItem = function(add){
+        this.items.push(add)
+    }
+    this.removeItem = function(remove){
+        const index = this.items.indexOf(remove)
+        this.items.splice(index, 1)
+    }
+    /* I created this render method as a demonstration of polymorphism this render method 
+    will take all items defined and create individual elements per value stated inside of a for loop.
+    before this occurs I created A parent element that will house each individual element inside of itself finally 
+    returning the complete element as the result */
+    this.render = function(){                                    
+        const element = document.createElement('select')        
+        for (item of this.items){
+            const childElement = document.createElement('option')
+            const elementContent = document.createTextNode(item)
+            childElement.appendChild(elementContent)
+            element.appendChild(childElement)
+        }
+        return element
+    }
+} 
+
+function HtmlImageSelect(sources){
+    HtmlSelectElement.call(this)
+    delete this.items
+    delete this.addItem
+    delete this.removeItem
+    this.sources = sources
+    this.render = function(){
+        this.sources = sources 
+        return `<img src="${this.sources}" />`
+    }
+}
+
+HtmlSelectElement.prototype = new HtmlElement /* correct implementation */
+
+HtmlImageSelect.prototype = new HtmlElement
+HtmlImageSelect.prototype.constructor = HtmlImageSelect
+
+const e = new HtmlElement
+
+const c = new HtmlSelectElement(1,2,3)
+
+const img = new HtmlImageSelect('https')
+
+HtmlSelectElement.prototype = new HtmlElement/* Incorrect implementation */
+
+const elements = [
+    new HtmlSelectElement(1,2,3),
+    new HtmlImageSelect('loser')
+];
+
+for (let element of elements){
+    console.log(element.render())
+}
+
+/* in this exercise I practiced creating my own protoypical inheritance,
+overall this exercise was fairly simple when it came to applying the prototypes of 
+individual constructor functions however I began to face problems when 
+attempting to assign HtmlSelectElements prototype to the HtmlElement 
+constructor function. This is because I had Defined the new prototype 
+after I had already defined c = new HtmlSelectElement. Forgetting
+that there is no hoisting in this scenario I was confused as to why the code
+wasn't working not realizing I had to just move it above the defined c
+object to fix this issue. My takeaway is the reminder that there is no hoisting
+for constructor functions and if i wish to reassign the prototype of a constructor
+function I must do it before defining the object containing said function */
+
+
 
 
 
