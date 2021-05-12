dacbd6ba3e33426e1fbc7793f96ef245e7abfc67
nichols20 <johnnytes10@gmail.com>
2021-05-11 23:00:52 -0400

    cleaned up code added it inside a class

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -1,20 +1,28 @@
 
 
-function getInput(){
-    const text = document.getElementById('input').value
-    console.log(text)
-    document.getElementById('input').value = ''
 
-    
-    const will = document.createElement('ul')
-    const content = document.createTextNode(text)
-    will.appendChild(content)
-    const remove = document.createElement('button')
-    const finished = document.createTextNode('done')
-    remove.appendChild(finished)
-    will.append(remove)
-    const div = document.getElementById('list')
-    div.appendChild(will)
-    
+  class toDO {
+      constructor(){
+          this.text = document.getElementById('input') /* retrieved the text input field and list div from html doc */
+          this.list = document.getElementById('list')
+      }
 
+      listToDo(){
+          /* once this function is called this creates a new ul element then adds the text input value to that element */
+          const newTask = document.createElement('ul')
+          const task = document.createTextNode(this.text.value)
+          newTask.append(task)
+
+          /* creates a button used fr deleting submitted tasks appended to each created task */
+          this.remove = document.createElement('button')
+          this.done = document.createTextNode('done')
+          this.remove.append(this.done)
+          newTask.append(this.remove)
+
+          /*adds new task to a div to be displayed on browser */
+          this.list.appendChild(newTask)
+          this.text.value = ''
+      }
   }
+
+  const toDoList = new toDO;
