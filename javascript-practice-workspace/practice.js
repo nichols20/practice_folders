ed8e7cb9819bfed86f13efa0a2a66269ae243ff4
nichols20 <johnnytes10@gmail.com>
2021-05-11 22:40:36 -0400

    display to do list in div added button with

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -4,4 +4,17 @@ function getInput(){
     const text = document.getElementById('input').value
     console.log(text)
     document.getElementById('input').value = ''
+
+    
+    const will = document.createElement('ul')
+    const content = document.createTextNode(text)
+    will.appendChild(content)
+    const remove = document.createElement('button')
+    const finished = document.createTextNode('done')
+    remove.appendChild(finished)
+    will.append(remove)
+    const div = document.getElementById('list')
+    div.appendChild(will)
+    
+
   }
