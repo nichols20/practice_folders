d2e120d1f5cb99c308751aa9149a85e201707440
nichols20 <johnnytes10@gmail.com>
2021-05-12 13:02:18 -0400

    added btn that deletes ul when task is completed

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -19,10 +19,19 @@
           this.remove.append(this.done)
           newTask.append(this.remove)
 
+
+          const div = this.list
+          this.remove.onclick = function(){
+            console.log('yes')
+            div.removeChild(newTask)
+        }
+
           /*adds new task to a div to be displayed on browser */
           this.list.appendChild(newTask)
           this.text.value = ''
       }
+
+      
   }
 
   const toDoList = new toDO;
