899341ebd109b3dc215967b0e36d39d6e62ed823
nichols20 <johnnytes10@gmail.com>
2021-05-13 00:14:00 -0400

    began calcultor project, added buttons & functs

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -1,37 +1,49 @@
 
 
+class Calculator {
+    constructor(){
+        this.one = document.getElementById('1')
+        this.two = document.getElementById('2')
+        this.three = document.getElementById('3')
+        this.four = document.getElementById('4')
+        this.five = document.getElementById('5')
+        this.six = document.getElementById('6')
+        this.seven = document.getElementById('7')
+        this.eight = document.getElementById('8')
+        this.nine = document.getElementById('9')
+        this.zero = document.getElementById('10')
+    }
 
-  class toDO {
-      constructor(){
-          this.text = document.getElementById('input') /* retrieved the text input field and list div from html doc */
-          this.list = document.getElementById('list')
-      }
+    selectedOne(){
+        console.log(this.one.textContent)
+    }
+    selectedTwo(){
+        console.log(this.two.textContent)
+    }
+    selectedThree(){
+        console.log(this.three.textContent)
+    }
+    selectedFour(){
+        console.log(this.four.textContent)
+    }
+    selectedFive(){
+        console.log(this.five.textContent)
+    }
+    selectedSix(){
+        console.log(this.six.textContent)
+    }
+    selectedSeven(){
+        console.log(this.seven.textContent)
+    }
+    selectedEight(){
+        console.log(this.eight.textContent)
+    }
+    selectedNine(){
+        console.log(this.nine.textContent)
+    }
+    selectedZero(){
+        console.log(this.zero.textContent)
+    }
+}
 
-      listToDo(){
-          /* once this function is called this creates a new ul element then adds the text input value to that element */
-          const newTask = document.createElement('ul')
-          const task = document.createTextNode(this.text.value)
-          newTask.append(task)
-
-          /* creates a button used fr deleting submitted tasks appended to each created task */
-          this.remove = document.createElement('button')
-          this.done = document.createTextNode('done')
-          this.remove.append(this.done)
-          newTask.append(this.remove)
-
-
-          const div = this.list
-          this.remove.onclick = function(){
-            console.log('yes')
-            div.removeChild(newTask)
-        }
-
-          /*adds new task to a div to be displayed on browser */
-          this.list.appendChild(newTask)
-          this.text.value = ''
-      }
-
-      
-  }
-
-  const toDoList = new toDO;
+const calculator = new Calculator
