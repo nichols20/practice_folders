d2023664a06ef61a0dd0332a751664b211843fc0
Andrice Nichols <johnnytes10@gmail.com>
2021-05-14 20:18:57 -0400

    added if statment to prevent no number input

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -14,60 +14,83 @@ class Calculator {
         this.zero = document.getElementById('0')
         this.plus = '+'
         this.assignedNumbers = []
+        this.input = document.getElementById('value') /* defined this.input to prevent
+        me from duplicating this code multiple times in other functions I can now just
+        call this object */
     }
 
     /*Majority of these functions below opperate for the individual button inputs to apply to the overall number 1,2,3...
     ///I'd like to find a way to simplify this function so I don't have to make one for each number on a calculator*/
 
     selectedOne(){
-        const input = document.getElementById('value')
-        const apply = document.createTextNode(this.one.textContent)
-        input.append(apply)
+        const apply = 1;
+        this.input.append(apply)
         console.log(this.one.textContent)
     }
     selectedTwo(){
-        const input = document.getElementById('value')
-        const apply = document.createTextNode(this.two.textContent)
-        input.append(apply)
+        const apply = 2;
+        this.input.append(apply)
         console.log(this.two.textContent)
     }
     selectedThree(){
+        const apply = 3;
+        this.input.append(apply)
         console.log(this.three.textContent)
     }
     selectedFour(){
+        const apply = 4;
+        this.input.append(apply)
         console.log(this.four.textContent)
     }
     selectedFive(){
+        const apply = 5;
+        this.input.append(apply)
         console.log(this.five.textContent)
     }
     selectedSix(){
+        const apply = 6;
+        this.input.append(apply)
         console.log(this.six.textContent)
     }
     selectedSeven(){
+        const apply = 7;
+        this.input.append(apply)
         console.log(this.seven.textContent)
     }
     selectedEight(){
+        const apply = 8;
+        this.input.append(apply)
         console.log(this.eight.textContent)
     }
     selectedNine(){
+        const apply = 9;
+        this.input.append(apply)
         console.log(this.nine.textContent)
     }
     selectedZero(){
+        const apply = 0;
+        this.input.append(apply)
         console.log(this.zero.textContent)
     }
     selectedPlus(){
-        const input = document.getElementById('value')
         /* the let object parses the input value the user inputs from a string to array*/
-        let trueNumber = parseFloat(input.textContent)
-        this.assignedNumbers.push(trueNumber)
-        input.textContent = ''
+        if(this.input.textContent == ''){
+            console.log('empty string')
+            return console.log('nope')
+        }
 
+
+        if(this.assignedNumbers.length >= 0){
+            let trueNumber = parseFloat(this.input.textContent)
+            this.assignedNumbers.push(trueNumber)
+        }
         /* Added this if statement that will reduce all items in the array to return one single sum*/
         if (this.assignedNumbers.length > 1){
             const value = this.assignedNumbers.reduce((a,b) => a + b)
             console.log(value)
         }
-        
+
+        this.input.textContent = ''
     }
 }
 
