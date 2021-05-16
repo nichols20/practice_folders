951ee3910dcf5a52e4be707e76bd5ef9d37c9e07
Andrice Nichols <johnnytes10@gmail.com>
2021-05-16 14:08:06 -0400

    fixed how the minus func worked,  added equals btn

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -1,5 +1,14 @@
+/* PROBLEMS TO FIX: 1. can't show the total values without applying that value 
+as an input. ex: 1 + 1 = 2 if i display two the next action would be 1+1+2|
+2. If i select the minus button after i've added or vice versa the array will reduce
+all the values again giving it a new value.  (It should be)ex: 1 - 1 + 2 = 2
+(what happens) ex: 1 + 1 + 2 = 4  */
 
-
+/* SOLUTION FOR 2. : from researching a way to detect button clicks inside a button
+click function I was able to determine there is no way to do such a thing becuase 
+clicking that next button will call the new function to perform. So what I did to
+solve this issue was apply a counter so that whenever minus was clicked it'd add +1
+that would then tell whatever number was selected to change over to a negative*/
 class Calculator {
     constructor(){
         this.one = document.getElementById('1')
@@ -12,8 +21,9 @@ class Calculator {
         this.eight = document.getElementById('8')
         this.nine = document.getElementById('9')
         this.zero = document.getElementById('0')
-        this.plus = '+'
+        this.value = 0
         this.assignedNumbers = []
+        this.minusClicked = 0
         /* defined this.input to prevent me from duplicating this code 
         multiple times in other functions I can now just call this object */
         this.input = document.getElementById('value')
@@ -23,53 +33,100 @@ class Calculator {
     /*Majority of these functions below opperate for the individual button inputs to apply to the overall number 1,2,3...
     ///I'd like to find a way to simplify this function so I don't have to make one for each number on a calculator*/
 
+
+
     selectedOne(){
-        const apply = 1;
+        if (this.minusClicked > 0){
+            let apply = -1
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 1;
         this.input.append(apply)
-        console.log(this.one.textContent)
     }
     selectedTwo(){
-        const apply = 2;
+        if (this.minusClicked > 0){
+            let apply = -2
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 2;
         this.input.append(apply)
-        console.log(this.two.textContent)
     }
     selectedThree(){
-        const apply = 3;
+        if (this.minusClicked > 0){
+            let apply = -3
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 3;
         this.input.append(apply)
-        console.log(this.three.textContent)
     }
     selectedFour(){
-        const apply = 4;
+        if (this.minusClicked > 0){
+            let apply = -4
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 4;
         this.input.append(apply)
-        console.log(this.four.textContent)
     }
     selectedFive(){
-        const apply = 5;
+        if (this.minusClicked > 0){
+            let apply = -5
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 5;
         this.input.append(apply)
-        console.log(this.five.textContent)
     }
     selectedSix(){
-        const apply = 6;
+        if (this.minusClicked > 0){
+            let apply = -6
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 6;
         this.input.append(apply)
-        console.log(this.six.textContent)
     }
     selectedSeven(){
-        const apply = 7;
+        if (this.minusClicked > 0){
+            let apply = -7
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 7;
         this.input.append(apply)
-        console.log(this.seven.textContent)
     }
     selectedEight(){
-        const apply = 8;
+        if (this.minusClicked > 0){
+            let apply = -8
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 8;
         this.input.append(apply)
-        console.log(this.eight.textContent)
     }
     selectedNine(){
-        const apply = 9;
+        if (this.minusClicked > 0){
+            let apply = -9
+            this.input.append(apply)
+            this.minusClicked = 0;
+            return
+        }
+        let apply = 9;
         this.input.append(apply)
-        console.log(this.nine.textContent)
     }
     selectedZero(){
-        const apply = 0;
+        let apply = 0;
         this.input.append(apply)
         console.log(this.zero.textContent)
     }
@@ -95,21 +152,65 @@ class Calculator {
 
     }
 
+    selectedEquals(){
+        if (this.assignedNumbers.length >= 0){
+            let trueNumber = parseFloat(this.input.textContent)
+
+            this.assignedNumbers.push(trueNumber)
+
+            this.value += this.assignedNumbers[0]
+            this.assignedNumbers.splice(0, this.assignedNumbers.length)
+        }
+        console.log(this.value)
+        this.value = 0
+        this.input.textContent = ''
+    }
+
+    selectedMinus(){
+
+       
+        if(this.input.textContent == ''){
+            return
+        }
+
+        
+        if (this.assignedNumbers.length >= 0){
+            let trueNumber = parseFloat(this.input.textContent)
+
+            this.assignedNumbers.push(trueNumber)
+
+            this.value += this.assignedNumbers[0]
+            console.log(this.value)
+            this.assignedNumbers.splice(0, this.assignedNumbers.length)
+        }
+        
+
+        if (this.assignedNumbers.length > 0){
+            this.clear.textContent = 'C'
+        }
+        else{
+            this.clear.textContent = 'AC'
+        }
+        this.minusClicked++
+
+        this.input.textContent = ''
+    }
+
     selectedPlus(){
         if(this.input.textContent == ''){
             return
         }
 
-
-        if(this.assignedNumbers.length >= 0){
+        if (this.assignedNumbers.length >= 0){
             /* the let object parses the input value the user inputs from a string to array*/
             let trueNumber = parseFloat(this.input.textContent)
             this.assignedNumbers.push(trueNumber)
-        }
-        /* Added this if statement that will reduce all items in the array to return one single sum*/
-        if (this.assignedNumbers.length > 1){
-            const value = this.assignedNumbers.reduce((a,b) => a + b)
-            console.log(value)
+           
+            this.value += this.assignedNumbers[0]
+            console.log(this.value)
+
+            this.assignedNumbers.splice(0, this.assignedNumbers.length)
+        
         }
 
         if (this.assignedNumbers.length > 0){
