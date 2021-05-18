8ec0cc7b03350eadf0e0e8ce0da83b3edfdcec4e
nichols20 <johnnytes10@gmail.com>
2021-05-18 13:31:34 -0400

    started creating saved note function

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -1,2 +1,18 @@
 
 
+class NoteApp{
+    constructor(){
+        this.noteContainer = document.getElementById('noteContainer')
+        this.newNote = document.getElementById('inputNote')
+    }
+
+    submitClicked(){
+        console.log(this.newNote.value)
+        this.newNote.value = ''
+        let savedNote = document.createElement('div')
+        this.noteContainer.append(savedNote)
+        console.log(savedNote)
+    }
+}
+
+const note = new NoteApp
