1d6bddf0bccbf48ee4db4a629f86b609f27f8f87
Andrice Nichols <johnnytes10@gmail.com>
2021-05-18 19:07:18 -0400

    append header once input submit is clicked

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -4,13 +4,17 @@ class NoteApp{
     constructor(){
         this.noteContainer = document.getElementById('noteContainer')
         this.newNote = document.getElementById('inputNote')
+        this.noteHeader = document.createElement('h4')
+        this.noteHeader.textContent ='New Note'
     }
 
     submitClicked(){
-        console.log(this.newNote.value)
-        this.newNote.value = ''
         let savedNote = document.createElement('div')
+        savedNote.append(this.noteHeader)
+
+
         this.noteContainer.append(savedNote)
+        this.newNote.value = ''
         console.log(savedNote)
     }
 }
