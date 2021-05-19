0f8def1a0bb79b343182b5d906b8925c7e2321b4
Andrice Nichols <johnnytes10@gmail.com>
2021-05-18 22:44:25 -0400

    started working on modal function- incomplete

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -1,12 +1,14 @@
 
-
 class NoteApp{
     constructor(){
         this.noteContainer = document.getElementById('noteContainer')
         this.newNote = document.getElementById('inputNote')
+        this.modal = document.getElementById('modal')
     }
 
     submitClicked(){
+        let modal = document.getElementById('modal')
+
         let noteHeader = document.createElement('h4')
         noteHeader.textContent ='New Note'
 
@@ -16,22 +18,29 @@ class NoteApp{
         deleteNote.onclick = function(){
             savedNote.remove()
         }
-        
-
-        let noteDetails = document.createElement('p')
-        noteDetails.textContent = this.newNote.value
 
         let viewDetails = document.createElement('button')
         viewDetails.textContent = 'View Details'
         viewDetails.id = 'viewDetails'
+        /*working progress */
         viewDetails.onclick = function(){
-            console.log('click')
+            modal.style.display = 'block'
+            let noteheader2 = noteHeader
+            let deleteNote2 = deleteNote
+    
+            let scopedNote = document.createElement('div')
+            scopedNote.id = 'scopedNote'
+            scopedNote.append(noteheader2, deleteNote2, noteDetails2)
+            modal.append(scopedNote)
+            
         }
+        /*///////////////////////////////////////////////////////////////////////*/
+        let noteDetails = document.createElement('p')
+        noteDetails.textContent = this.newNote.value
 
         let savedNote = document.createElement('div')
         savedNote.id = 'savedNote'
         savedNote.append(noteHeader, deleteNote, noteDetails, viewDetails)
-
 
         this.noteContainer.append(savedNote)
         this.newNote.value = ''
