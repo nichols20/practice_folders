a298d5e15b2e81a47e1147edde6de39cbd176ea5
Andrice Nichols <johnnytes10@gmail.com>
2021-05-18 20:03:04 -0400

    added btns- delete created note & view Detail btn

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -4,19 +4,39 @@ class NoteApp{
     constructor(){
         this.noteContainer = document.getElementById('noteContainer')
         this.newNote = document.getElementById('inputNote')
-        this.noteHeader = document.createElement('h4')
-        this.noteHeader.textContent ='New Note'
     }
 
     submitClicked(){
+        let noteHeader = document.createElement('h4')
+        noteHeader.textContent ='New Note'
+
+        let deleteNote = document.createElement('button')
+        deleteNote.textContent = 'x'
+        deleteNote.id = 'deleteNote'
+        deleteNote.onclick = function(){
+            savedNote.remove()
+        }
+        
+
+        let noteDetails = document.createElement('p')
+        noteDetails.textContent = this.newNote.value
+
+        let viewDetails = document.createElement('button')
+        viewDetails.textContent = 'View Details'
+        viewDetails.id = 'viewDetails'
+        viewDetails.onclick = function(){
+            console.log('click')
+        }
+
         let savedNote = document.createElement('div')
-        savedNote.append(this.noteHeader)
+        savedNote.id = 'savedNote'
+        savedNote.append(noteHeader, deleteNote, noteDetails, viewDetails)
 
 
         this.noteContainer.append(savedNote)
         this.newNote.value = ''
-        console.log(savedNote)
     }
+
 }
 
 const note = new NoteApp
