1098d87d44140b1dd0976617edd13a6f8d7879b3
nichols20 <johnnytes10@gmail.com>
2021-05-21 13:03:54 -0400

    implemented individual picture & tile vars

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -2,11 +2,18 @@
 class Memory {
     constructor(){
         this.tile = document.getElementById('memoryTile1')
+        this.tilePic = document.getElementById('tile1Pic')
+        this.source = document.getElementById('tile1Pic').src
+        this.match = []
         this.clickAmount = 0 /*once two clicks have occured the program will analyze whether the two images are the same*/
     }
 
-    tileClicked(){
-        this.tile.style.opacity = '0'
+    tile1Clicked(){
+        this.tile.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+        this.tilePic.style.opacity = '1'
+        console.log(this.source)
+        this.match.push(this.source)
+        console.log(this.match)
         this.clickAmount++
     }
 }
