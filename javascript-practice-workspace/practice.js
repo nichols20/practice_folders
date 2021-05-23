bb4821a4230e5ac23222ed42a07161962052bbb7
Andrice Nichols <johnnytes10@gmail.com>
2021-05-23 01:46:42 -0400

    added more work unfinished

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -7,6 +7,12 @@ class Memory {
         this.tile2 = document.getElementById('memoryTile2')
         this.tilePic2 = document.getElementById('tile2Pic')
         this.source2 = document.getElementById('tile2Pic').src
+        this.tile3 = document.getElementById('memoryTile3')
+        this.tilePic3 = document.getElementById('tile3Pic')
+        this.source3 = document.getElementById('tile3Pic').src
+        this.tile4 = document.getElementById('memoryTile4')
+        this.tilePic4 = document.getElementById('tile4Pic')
+        this.source4 = document.getElementById('tile4Pic').src
         this.match = []
         this.clickAmount = 0 /*once two clicks have occured the program will analyze whether the two images are the same*/
 
@@ -31,6 +37,24 @@ class Memory {
             this.clickAmount++
             this.checkMatch()
         }
+        this.tile3Clicked = function(){
+            this.tile3.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+            this.tilePic3.style.opacity = '1'
+    
+            this.match.push(this.source3)
+
+            this.clickAmount++
+            this.checkMatch()
+        }
+        this.tile4Clicked = function(){
+            this.tile4.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+            this.tilePic4.style.opacity = '1'
+    
+            this.match.push(this.source4)
+
+            this.clickAmount++
+            this.checkMatch()
+        }
     }
 
     checkMatch(){
@@ -38,6 +62,47 @@ class Memory {
       if(this.clickAmount === 2){
         if(this.match[0] === this.match[1]){
             window.alert('nice job!')
+            this.clickAmount = 0;
+        }
+        else if(this.match[0] !== this.match[1]){
+            this.clickAmount = 0;
+            window.alert('try again :(')
+            if(this.match[0] === this.source){
+                this.tile.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic.style.opacity = '0'
+            }
+            if(this.match[0] === this.source2){
+                this.tile2.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic2.style.opacity = '0'
+            }
+            if(this.match[0] === this.source3){
+                this.tile3.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic3.style.opacity = '0'
+            }
+            if(this.match[0] === this.source4){
+                this.tile4.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic4.style.opacity = '0'
+            }
+            if(this.match[1] === this.source){
+            this.tile.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic.style.opacity = '0'
+                this.match.splice(0, 2)
+            }
+            if(this.match[1] === this.source2){
+                this.tile2.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic2.style.opacity = '0'
+                this.match.splice(0, 2)
+            }
+            if(this.match[1] === this.source3){
+                this.tile3.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic3.style.opacity = '0'
+                this.match.splice(0, 2)
+            }
+            if(this.match[1] === this.source4){
+                this.tile4.style.backgroundColor = 'rgba(116, 59, 59, 1)'
+                this.tilePic4.style.opacity = '0'
+                this.match.splice(0, 2)
+            }
         }
       }
     }        
