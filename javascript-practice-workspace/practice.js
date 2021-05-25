6143cd2313fb7147fa42e984e6b8e6569728660a
Andrice Nichols <johnnytes10@gmail.com>
2021-05-25 10:59:00 -0400

    made it so double selected tiles won't be match

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -14,13 +14,21 @@ class Memory {
         this.tilePic4 = document.getElementById('tile4Pic')
         this.source4 = document.getElementById('tile4Pic').src
         this.match = []
+        this.chosen1 = 0
+        this.chosen2 = 0
+        this.chosen3 = 0
+        this.chosen4 = 0
         this.clickAmount = 0 /*once two clicks have occured the program will analyze whether the two images are the same*/
-        this.countCorrect = 0         
+        this.countCorrect = 0   
+        this.kowalaMatched = 0
+        this.giraffeMatched = 0      
         this.text = document.createElement('p')         
         this.text.innerHTML = `Score: ${this.countCorrect}`        
         document.body.append(this.text)
 
         this.tile1Clicked = function(){
+            if (this.chosen1 > 0 || this.giraffeMatched > 0)return
+
             /*reveals card selected */
             this.tile.style.backgroundColor = 'rgba(0, 0, 0, 0)'
             this.tilePic.style.opacity = '1'
@@ -28,56 +36,65 @@ class Memory {
             /*pushes source of image selected to empty array */
             this.match.push(this.source)
 
+            this.chosen1++
             /*increments click amount +1 then checks if if statement in checkMatch() is true */
             this.clickAmount++
             this.checkMatch()
         }
         this.tile2Clicked = function(){
+            if (this.chosen2 > 0 || this.giraffeMatched > 0)return
             this.tile2.style.backgroundColor = 'rgba(0, 0, 0, 0)'
             this.tilePic2.style.opacity = '1'
     
             this.match.push(this.source2)
 
+            this.chosen2++
             this.clickAmount++
             this.checkMatch()
         }
         this.tile3Clicked = function(){
+            if (this.chosen3 > 0 || this.kowalaMatched > 0)return
             this.tile3.style.backgroundColor = 'rgba(0, 0, 0, 0)'
             this.tilePic3.style.opacity = '1'
     
             this.match.push(this.source3)
-
+            
+            this.chosen3++
             this.clickAmount++
             this.checkMatch()
         }
         this.tile4Clicked = function(){
+            console.log('this is it')
+            if (this.chosen4 > 0 || this.kowalaMatched > 0)return
             this.tile4.style.backgroundColor = 'rgba(0, 0, 0, 0)'
             this.tilePic4.style.opacity = '1'
     
             this.match.push(this.source4)
 
+            this.chosen4++
             this.clickAmount++
             this.checkMatch()
         }
     }
 
     checkMatch(){
-      console.log('yes')
       if(this.clickAmount === 2){
         if(this.match[0] === this.match[1]){
             window.alert('nice job!')
+            if(this.match[0] === this.source3)this.chosen3 = 0, this.chosen4 = 0, this.kowalaMatched++
+            if(this.match[0] === this.source)this.chosen1 = 0, this.chosen2 = 0, this.giraffeMatched++
             this.clickAmount = 0;
             this.countCorrect++
             this.text.innerHTML = `Score: ${this.countCorrect}`
 
             this.match.splice(0, 2)
-
-            if(this.match[0] === this.source){
-                this.tile.onclick = 'whatever'
-            }
         }
         else if(this.match[0] !== this.match[1]){
             this.clickAmount = 0;
+            this.chosen1 = 0;
+            this.chosen2 = 0;
+            this.chosen3 = 0;
+            this.chosen4 = 0;
             alert('try again :(')
             if(this.match[0] === this.source){
                 this.tile.style.backgroundColor = 'rgba(116, 59, 59, 1)'
