dba8b66082fcb06ad15923e78c9dc2347dbd2b12
nichols20 <johnnytes10@gmail.com>
2021-06-09 20:34:09 -0400

    automation roads exercise

--- javascript-practice-workspace/practice.js
+++ javascript-practice-workspace/practice.js
@@ -1,1 +1,35 @@
- 
+ const roads = [
+     `Alice's House-Bob's House`, `Alice's House-Cabin`,
+     `Alice's House-Post Office`, `Bob's House-Town Hall`,
+     `Daria's House-Ernie's House`, `Daria's House-Town Hall`,
+     `Ernie's House-Grete's House`, `Grete's House-Farm`,
+     `Grete's House-Shop`, `Marketplace-Farm`,
+     `Marketplace-Post Office`, `Marketplace-Shop`,
+        
+    ]
+ function buildGraph(edges) {
+    let graph = Object.create(null)
+    function addEdge(from, to){
+        if (graph[from] == null){
+            graph[from] = [to];
+            console.log(graph[from])
+        }
+        else {
+            //graph[from].push(to)
+            //console.log(graph)
+        }
+    }
+
+    for (let [from, to] of edges.map( r => r.split('-'))){
+        addEdge(from, to)
+        addEdge(to, from)
+    }
+    return graph
+} 
+
+
+
+const roadGraph = buildGraph(roads)
+
+
+
